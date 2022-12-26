4a645fc8bfc893c21b3b6a60dd120bb280b18a32
Rajiv Kumar Singh <118833528+R-ajiv@users.noreply.github.com>
2022-12-26 08:44:03 +0530

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,9 @@
+2278. Percentage of Letter in String
+
+class Solution:
+    def percentageLetter(self, s: str, letter: str) -> int:
+        c=0
+        for i in letter:
+            if i in s:
+                c+=s.count(i)
+        return c*100//len(s)
