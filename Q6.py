e60b29267a4fad76d59081b90b190fde5daadd66
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-26 08:48:28 +0530

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -2,8 +2,8 @@
 
 class Solution:
     def percentageLetter(self, s: str, letter: str) -> int:
-        c=0
-        for i in letter:
-            if i in s:
-                c+=s.count(i)
+#         c=0
+#         for i in letter:
+#             if i in s:
+        c=s.count(letter)
         return c*100//len(s)
